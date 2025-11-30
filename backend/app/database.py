f38a96428aa8da8d2aebe7a82b159c718978f481
Vladislav Kobzev <kvladislav1305@gmail.com>
2025-11-30 17:37:13 +0500

    init

---
+++ backend/app/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from .config import settings
+
+engine = create_engine(
+    url=settings.database_url,
+    connect_args={"check_same_thread": False}
+)
+
+class Base(DeclarativeBase):
+    pass
+
+
+SessionLocal = sessionmaker(engine, autoflush=False)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def init_db():
+    Base.metadata.create_all(engine)
+
